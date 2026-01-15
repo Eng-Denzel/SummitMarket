42291c150eb62f63f4f594709b8fa0821b8bbf8b
Eng-Denzel <130589031+Eng-Denzel@users.noreply.github.com>
2026-01-15 15:24:50 +0100

    updates

--- Backend/api/admin_views.py
+++ Backend/api/admin_views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from django.contrib.auth.models import User
-from django.db.models import Sum, Count, Q
+from django.db.models import Sum, Count, Q, F
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from datetime import datetime, timedelta
